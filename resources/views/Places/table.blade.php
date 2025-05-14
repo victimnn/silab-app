9843ec5b57c1ad1d001dde8b185726c7fa7fdab6
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2025-05-14 12:13:33 -0300

    aaa

--- resources/views/Places/table.blade.php
+++ resources/views/Places/table.blade.php
@@ -33,12 +33,9 @@
                         <td>{{ $place->description }}</td>
                         <td class="d-flex gap-1">
                             <a href="/places/{{$place->id}}/edit" class="btn btn-secondary">Editar</a>
-
-                                <form action="/places/{{$place->id}}" method="POST">
-                                    @method('delete')
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger">Excluir</button>
-                                </form>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationModal" data-place-id="{{ $place->id }}">
+                                    Excluir
+                                </button>
                         </td>
                     </tr>
 
@@ -55,6 +52,45 @@
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h1 class="modal-title fs-5 fw-bold" id="confirmationModalLabel">🚨Alerta</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+            <div class="modal-body">
+                Confirmar Exclusão?
+            </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="btnVoltar">Voltar</button>
+                    <form id="deletePlaceForm" method="POST">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Excluir</button>
+                    </form>
+                </button>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
 
+@section('scripts')
+    
+    <script>
+        const confirmationModal = document.getElementById('confirmationModal')
+        const form = document.getElementById('deletePlaceForm')
+        const btnVoltar = document.getElementById('btnVoltar')
+
+        confirmationModal.addEventListener('show.bs.modal', (event) => {
+            btnVoltar.focus()
+
+            const id = (event.relatedTarget.getAttribute('data-place-id'));
+
+            form.action = `/places/${id}`
+        })
+    </script>
 
 @endsection
