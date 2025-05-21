bc761fb70925dc3f83ebd55eaf9cdcd362a535c9
Victor Ramos <126915227+victimnn@users.noreply.github.com>
2025-05-21 11:35:54 -0300

    aula

--- resources/views/Places/table.blade.php
+++ resources/views/Places/table.blade.php
@@ -143,15 +143,7 @@
 @endsection
 
 @section('scripts')
-    <script>
-        const confirmationModal = document.getElementById('confirmationModal')
-        const form = document.getElementById('deletePlaceForm')
-        const btnVoltar = document.getElementById('btnVoltar')
-
-        confirmationModal.addEventListener('show.bs.modal', (event) => {
-            btnVoltar.focus()
-            const id = (event.relatedTarget.getAttribute('data-place-id'));
-            form.action = `/places/${id}`
-        })
+    <script src="{{asset('js/confirmationModal.js')}}"></script>
+        
     </script>
 @endsection
